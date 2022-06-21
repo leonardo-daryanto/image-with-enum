fdd502c288aa0f8938993c2f2e0536979de144cb
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-06-21 11:00:03 +0700

    Update pxt.json, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-basic.forever(function () {
-	
-})
+enum Images {
+    //% block="snake"
+    One,
+    //% block="Check"
+    Two,
+}
+
+//% weight=100 color=#0E6655 icon=I
+namespace images {
+    /**
+     * make a image with enum, e.g. Snake, Check.
+     */
+    //% block
+    export function makeImages(image: Images): void {
+          
+        if (image == 1) {
+            
+            basic.showIcon(IconNames.Snake)
+        }
+        
+        if (image == 2) {
+            
+            basic.showIcon(IconNames.Yes)
+        }
+    }
+}
